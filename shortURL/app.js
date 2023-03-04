9f7c48efb35492b051ace995d2abe99e02c2c721
Pikapikapi <huang.paladin@gmail.com>
2023-03-04 15:49:50 +0800

    feat: add handlebars

--- shortURL/app.js
+++ shortURL/app.js
@@ -1,10 +1,13 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
 const router = express.Router()
 const bodyParser = require('body-parser')
 const routes = require('./routes')
 const port = 3000
 
 const app = express()
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(routes)
 
