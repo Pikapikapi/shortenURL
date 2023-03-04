e4bdf3f4cb4cd423840aa79f29f3e8899d4a43bf
Pikapikapi <huang.paladin@gmail.com>
2023-03-04 18:29:13 +0800

    feat: result function added

--- shortURL/routes/modules/result.js
+++ shortURL/routes/modules/result.js
@@ -2,4 +2,15 @@ const express = require('express')
 const router = express.Router()
 const UrlModel = require('../../models/shortURLMap')
 
+router.get('/:shortUrl', async (req, res) => {
+  const shorten = req.params.shortUrl
+  await UrlModel.findOne({ shortUrl: shorten }, (err, doc) => {
+    if (doc) {
+      res.redirect(doc.longUrl)
+    } else {
+      res.send('Invalid short URL')
+    }
+  })
+})
+
 module.exports = router
